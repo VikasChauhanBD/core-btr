b8a4958e9f1cdb0839383988d12dc175030472b5
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-03-16 16:38:25 +0530

    initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header-container">
+      <h1>Manage Your Health Anytime, Anywhere</h1>
+      <p>
+        Curely helps you book appointments, track wellness, consult doctors, and
+        manage reports – all in one simple app.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
