6721c1509a8223f7b62e5fa62891a9f55a1b0c81
vamikazvm <vamikazutshi@gmail.com>
2026-03-17 13:46:54 +0530

    privacy policy page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/HomePage";
+import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -12,15 +13,15 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="/about-books" element={<AboutUsPage />} />
-          <Route path="/plans" element={<CRBooksPage />} />
-          <Route path="/reviews" element={<CrispPage />} />
-          <Route path="/gallery" element={<StudentsPage />} />
-          <Route path="/privacy-policy" element={<GalleryPage />} />
-          <Route path="/pricing-details" element={<FaqsPage />} />
-          <Route path="/terms-and-conditions" element={<StudentsPage />} />
-          <Route path="/cancellation-and-refund-policy" element={<GalleryPage />} />
-          <Route path="/shipping-and-delivery-policy" element={<FaqsPage />} /> */}
+          {/* <Route path="/about-books" element={<AboutUsPage />} /> */}
+          {/* <Route path="/plans" element={<CRBooksPage />} /> */}
+          {/* <Route path="/reviews" element={<CrispPage />} /> */}
+          {/* <Route path="/gallery" element={<StudentsPage />} /> */}
+          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          {/* <Route path="/pricing-details" element={<FaqsPage />} /> */}
+          {/* <Route path="/terms-and-conditions" element={<StudentsPage />} /> */}
+          {/* <Route path="/cancellation-and-refund-policy" element={<GalleryPage />} /> */}
+          {/* <Route path="/shipping-and-delivery-policy" element={<FaqsPage />} /> */}
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/pages/PrivacyPolicyPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PrivacyPolicy from "../components/Policies/PrivacyPolicy";
+
+function PrivacyPolicyPage() {
+  return (
+    <div>
+      <PrivacyPolicy />
+    </div>
+  );
+}
+
+export default PrivacyPolicyPage;
