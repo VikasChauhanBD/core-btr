5bfdc1f1bdce47bcceda32fc213fe7064f789a0d
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-03-21 10:37:45 +0530

    unused code removed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import AboutBooks from "./pages/AboutBooks";
 import Annotated from "./components/annotated/Annotated";
 import Unannotated from "./components/unannotated/Unannotated";
 import LoadingScreen from "./components/loadingScreen/LoadingScreen";
-import PlansPage from "./pages/PlansPage";
 import BTRonline from "./components/btr-online/BTRonline";
 import BTRoffline from "./components/btr-offline/BTRoffline";
 import Bootcamp from "./components/bootcamp/Bootcamp";
@@ -63,17 +62,14 @@ function App() {
           <Route path="/about-books" element={<AboutBooks />} />
           <Route path="/annotated" element={<Annotated />} />
           <Route path="/unannotated" element={<Unannotated />} />
-          <Route path="/plans" element={<PlansPage />} />
           <Route path="/btr-online" element={<BTRonline />} />
           <Route path="/btr-offline" element={<BTRoffline />} />
           <Route path="/bootcamp" element={<Bootcamp />} />
           <Route path="/reviews" element={<StudentReviewsPage />} />
-          {/* <Route path="/gallery" element={<StudentsPage />} /> */}
           <Route path="/faqs" element={<FaqsPage />} />
 
           {/* ------------------------ privacy Routes --------------------------- */}
           <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-          {/* <Route path="/pricing-details" element={<FaqsPage />} /> */}
           <Route
             path="/terms-and-conditions"
             element={<TermsAndConditions />}
@@ -82,6 +78,7 @@ function App() {
             path="/cancellation-and-refund-policy"
             element={<CancellationPolicy />}
           />
+          {/* <Route path="/pricing-details" element={<FaqsPage />} /> */}
           {/* <Route path="/shipping-and-delivery-policy" element={<FaqsPage />} /> */}
         </Routes>
         <Footer />
